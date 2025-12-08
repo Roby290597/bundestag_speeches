f35e114fe4647c09439efcfae386f7461611d213
Roby290597 <brhan100@hhu.de>
2025-12-08 10:56:57 +0100

    Fine-tuning text summarization model

--- fine_tune_summarization.py
+++ fine_tune_summarization.py
@@ -1,3 +1,7 @@
+########################################################
+#####  Imports and tokenizer setup  ####################
+########################################################
+
 from transformers import AutoTokenizer
 
 checkpoint = "google-t5/t5-small"
@@ -17,15 +21,38 @@ def preprocess_function(examples):
     return model_inputs
 
 
-### 
+# import torch
+# from transformers import BertTokenizerFast, EncoderDecoderModel
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# ckpt = 'mrm8488/bert2bert_shared-german-finetuned-summarization'
+# tokenizer = BertTokenizerFast.from_pretrained(ckpt)
+# model = EncoderDecoderModel.from_pretrained(ckpt).to(device)
+# def generate_summary(text):
+#    inputs = tokenizer([text], padding="max_length", truncation=True, max_length=512, return_tensors="pt")
+#    input_ids = inputs.input_ids.to(device)
+#    attention_mask = inputs.attention_mask.to(device)
+#    output = model.generate(input_ids, attention_mask=attention_mask)
+#    return tokenizer.decode(output[0], skip_special_tokens=True)
+   
+# text = "Your text here..."
+
+# generate_summary(text)
+'
+
+
+################################################################################
+##### Model creation using the checkpoint ######################################
+################################################################################
+
 
 from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
 
 model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 
-########################################
-##### Load the dataset for fine-tuning 
-########################################
+################################################################################
+##### Load the dataset for fine-tuning #########################################
+################################################################################
+
 
 from datasets import load_dataset
 
